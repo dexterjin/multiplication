3b926143f35944f2f054d5835d52db20e8b8f348
Sungjin-Wie <tmdwls1203@gmail.com>
2020-10-16 10:30:30 +0900

    [Add] 9단

--- test.c
+++ test.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void)
 {
-	int i,j;
+	int i, j;
 
-	for (i = 1; i < 9; i++) 
-		for (j = 1; j < 9; j++)
-			printf("%d * %d = %d\n",i, j, i*j);
+	for (i = 1; i <= 9; i++)
+		for (j = 1; j <= 9; j++)
+			printf("%d * %d = %d\n", i, j, i * j);
 
 	printf("Hello World!\n");
 	printf("Hello World!\n");
